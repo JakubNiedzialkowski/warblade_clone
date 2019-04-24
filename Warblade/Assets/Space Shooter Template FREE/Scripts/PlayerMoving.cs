e06eb7159e095f1c83717647b34ea9ea50f7b445
Jakub_Niedzialkowski <jakubniedzialkowski97@tlen.pl>
2019-04-24 21:54:47 +0200

    added mobile controls

---
+++ Warblade/Assets/Space Shooter Template FREE/Scripts/PlayerMoving.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+/// <summary>
+/// This script defines the borders of ‘Player’s’ movement. Depending on the chosen handling type, it moves the ‘Player’ together with the pointer.
+/// </summary>
+
+[System.Serializable]
+public class Borders
+{
+    [Tooltip("offset from viewport borders for player's movement")]
+    public float minXOffset = 1.5f, maxXOffset = 1.5f, minYOffset = 1.5f, maxYOffset = 1.5f;
+    [HideInInspector] public float minX, maxX, minY, maxY;
+}
+
+public class PlayerMoving : MonoBehaviour {
+
+    [Tooltip("offset from viewport borders for player's movement")]
+    public Borders borders;
+    Camera mainCamera;
+    bool controlIsActive = true;
+    public Rigidbody2D rb;
+    private float dirX;
+    private float moveSpeed = 5f;
+
+    public static PlayerMoving instance; //unique instance of the script for easy access to the script
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        ResizeBorders();                //setting 'Player's' moving borders deending on Viewport's size
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (controlIsActive)
+        {
+                dirX = CrossPlatformInputManager.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+                transform.position = new Vector3(transform.position.x+dirX, transform.position.y , 0f);
+
+            transform.position = new Vector3    //if 'Player' crossed the movement borders, returning him back 
+                (
+                Mathf.Clamp(transform.position.x, borders.minX, borders.maxX),
+                Mathf.Clamp(transform.position.y, borders.minY, borders.maxY),
+                0
+                );
+        }
+    }
+
+    //setting 'Player's' movement borders according to Viewport size and defined offset
+    void ResizeBorders() 
+    {
+        borders.minX = mainCamera.ViewportToWorldPoint(Vector2.zero).x + borders.minXOffset;
+        borders.minY = mainCamera.ViewportToWorldPoint(Vector2.zero).y + borders.minYOffset;
+        borders.maxX = mainCamera.ViewportToWorldPoint(Vector2.right).x - borders.maxXOffset;
+        borders.maxY = mainCamera.ViewportToWorldPoint(Vector2.up).y - borders.maxYOffset;
+    }
+}
